982d7c8dfa4d593f9104c4133b8c337031a59a9e
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2023-03-19 21:24:03 -0300

    added parser first interface

---
+++ AST/src/main/kotlin/types/PrintExpression.kt
@@ -0,0 +1,10 @@
+package types
+
+import interfaces.ASTNode
+import interfaces.ASTNodeVisitor
+
+class PrintExpression: ASTNode {
+    override fun accept(visitor: ASTNodeVisitor) {
+        visitor.visitPrint(this)
+    }
+}
