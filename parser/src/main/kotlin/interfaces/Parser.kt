c73a60987153f32efbc7a7881a64d50b6a9f19b2
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2023-03-21 21:26:36 -0300

    create hook command working

--- parser/src/main/kotlin/interfaces/Parser.kt
+++ parser/src/main/kotlin/interfaces/Parser.kt
@@ -1,7 +1,7 @@
 package interfaces
 
 interface Parser {
-                                                                                                                                    fun getASTTree(tokens: List<Token>): ASTNode
+                            fun getASTTree(tokens: List<Token>): ASTNode
 }
 
 data class Token(val a: String) // TODO delete when lexer is ready
