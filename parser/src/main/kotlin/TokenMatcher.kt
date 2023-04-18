013d9a501a8d5eaea484316202a2b10423d468d7
Nacho <72108528+Nachoferra1894@users.noreply.github.com>
2023-04-17 23:22:45 -0300

    Feat/cli (#7)
    * CLI working
    
    * fix cli option
    
    * added functions
    
    * formatting
    
    * added exec cli
    
    * fixed spaces
    
    * moved to global variables
    
    * added test

--- parser/src/main/kotlin/TokenMatcher.kt
+++ parser/src/main/kotlin/TokenMatcher.kt
@@ -5,16 +5,16 @@ import kotlin.jvm.Throws
 
 abstract class TokenMatcher(private val tokens: List<Token>) {
     @Throws(WrongTokenException::class, NoEOLException::class)
-    fun getNextTokenOrThrowError(index: Int, tokenTypes: List<PrototypeType>): Token {
+    fun getNextTokenOrThrowError(index: Int, tokenTypes: List<PrototypeType>): Pair<Token, Int> {
         if (index >= tokens.size) throw NoEOLException()
         val nextToken = tokens[index]
         if (nextToken.prototypeType === PrototypeType.SPACE) return getNextTokenOrThrowError(index + 1, tokenTypes)
         if (!tokenTypes.contains(nextToken.prototypeType)) throw WrongTokenException(nextToken)
-        return nextToken
+        return Pair(nextToken, index + 1)
     }
 
     @Throws(WrongTokenException::class)
-    fun getNextTokenOrThrowError(index: Int, tokenType: PrototypeType): Token {
+    fun getNextTokenOrThrowError(index: Int, tokenType: PrototypeType): Pair<Token, Int> {
         return getNextTokenOrThrowError(index, listOf(tokenType))
     }
 }
