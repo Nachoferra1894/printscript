2e3584c8e5dbe7fa781da036b025ab7c47ee04f5
micaeladominguez <micaela.dominguez@ing.austral.edu.ar>
2023-04-22 16:15:33 -0300

    Fix lexer (#18)
    Co-authored-by: Micaela Dominguez <micadom9@gmail.com>

--- lexer/src/main/kotlin/strategies/ValueStrategy.kt
+++ lexer/src/main/kotlin/strategies/ValueStrategy.kt
@@ -5,12 +5,14 @@ import lexer.exceptions.NoTokenException
 
 class ValueStrategy : Strategy {
     override fun isStrategy(line: String, index: Int): Boolean {
-        return line[index] == '"' || line[index].isDigit() || isTrue(line, index) || isFalse(line, index)
+        return line[index] == '"' || line[index].toString() == "'" ||
+            line[index].isDigit() || isTrue(line, index) ||
+            isFalse(line, index)
     }
 
     override fun getToken(line: String, index: Int, lineIndex: Int): Token {
         if (line[index].isDigit()) return valueNumber(line, index, lineIndex)
-        if (line[index] == '"') return valueString(line, index, lineIndex)
+        if (line[index] == '"' || line[index].toString() == "'") return valueString(line, index, lineIndex)
         if (isTrue(line, index)) return valueTrue(index, lineIndex)
         if (isFalse(line, index)) return valueFalse(index, lineIndex)
         throw NoTokenException("No value exists with this operator " + line[index])
@@ -26,7 +28,7 @@ class ValueStrategy : Strategy {
     private fun valueNumber(line: String, index: Int, lineIndex: Int): Token {
         var number: String = line[index].toString()
         var actualIndex: Int = index + 1
-        while (actualIndex < line.length - 1 && line[actualIndex] != ' ' || line[actualIndex] != ';' && !TokenStrategy.finalStrategy(line, index)) {
+        while (actualIndex < line.length - 1 && (line[actualIndex].isDigit() || line[actualIndex] == ',') && !TokenStrategy.finalStrategy(line, index)) {
             number.plus(line[actualIndex].toString()).also { number = it }
             actualIndex = actualIndex.plus(1)
         }
@@ -45,7 +47,7 @@ class ValueStrategy : Strategy {
         var value = ""
         var isClosed = false
         for (i in index + 1 until line.length) {
-            if (line[i] == '"') isClosed = true
+            if (line[i] == '"' || line[i].toString() == "'") isClosed = true
             if (isClosed) break
             if (TokenStrategy.finalStrategy(line, i) && !isClosed) {
                 throw NoTokenException("No value exists with this operator " + line[i])
