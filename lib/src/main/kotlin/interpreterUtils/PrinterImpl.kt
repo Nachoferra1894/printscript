3ffbdec82f70fc91317474727a9dc68d20fdbdd7
nachobattolla <ignacio.fbattolla@ing.austral.edu.ar>
2023-07-03 12:10:42 -0300

    Agregado de messsages al PrinterImpl

--- lib/src/main/kotlin/interpreterUtils/PrinterImpl.kt
+++ lib/src/main/kotlin/interpreterUtils/PrinterImpl.kt
@@ -1,7 +1,16 @@
 package interpreterUtils
 
 class PrinterImpl : Printer {
+
+    private val messages = ArrayList<String>()
+
     override fun print(value: String) {
+        messages.add(value)
         println(value)
     }
+
+    fun getMessages(): List<String?>? {
+        return messages
+    }
+
 }
