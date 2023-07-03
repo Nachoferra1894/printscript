bb5c22aee73c804dec6ffa18a04213e1fb381b1a
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2023-07-03 10:44:37 -0300

    fixed interpreter print issue

--- interpreter/src/main/kotlin/implementation/v1/InterpreterVisitorV1.kt
+++ interpreter/src/main/kotlin/implementation/v1/InterpreterVisitorV1.kt
@@ -214,7 +214,13 @@ class InterpreterVisitorV1(
     }
 
     override fun visitPrint(printNode: PrintNode) {
-        val stringValue: String = visitExpressionNode(printNode.content).toString()
+        var stringValue: String = visitExpressionNode(printNode.content).toString()
+        if (stringValue[0] == '"') {
+            stringValue = stringValue.substring(1, stringValue.length - 1)
+        }
+        if (stringValue[stringValue.length - 1] == '"') {
+            stringValue = stringValue.substring(0, stringValue.length - 2)
+        }
         printer.print(stringValue.replace(".0", ""))
     }
 
